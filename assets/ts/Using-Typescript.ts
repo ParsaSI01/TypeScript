552b0de59bd67017b31d1d9c7e21e4e3280c4087
ParsaSI <parsasi0101@gmail.com>
2026-08-25 01:03:23 +0330

    Using Typescript

---
+++ assets/ts/Using-Typescript.ts
@@ -0,0 +1,6 @@
+// =================================================
+// Using TypeScript
+// Compile: npx tsc assets/ts/Using-Typescript.ts
+// =================================================
+
+console.log('My Code Will Run Here...');
