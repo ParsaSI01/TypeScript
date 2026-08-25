c726e86be63241e9579707a12895c7c62f990f1f
ParsaSI <parsasi0101@gmail.com>
2026-08-25 18:25:54 +0330

    ObjectTypes

---
+++ assets/js/ObjectTypes.js
@@ -0,0 +1,40 @@
+"use strict";
+// ===========================================
+// ObjectTypes
+// ===========================================
+/* let person = {
+    name: 'John Doe',
+    age: 22,
+    isMale: true,
+};
+
+console.log(person); */
+let person;
+person = {
+    name: 'John Doe',
+    age: 22,
+    isMale: true,
+};
+console.log(person);
+// console.log(person.name); // We Cant Access The Properties Unless We Declare They're Types
+let person2;
+person2 = {
+    name: 'Jane Doe',
+    age: 20,
+    isMale: false,
+};
+// We Can Also Do This
+/* let person2: {
+    name: string;
+    age: number;
+    isMale: boolean;
+} = {
+    name: 'Jane Doe',
+    age: 20,
+    isMale: false,
+};
+ */
+console.log(person2);
+console.log(person2.name);
+console.log(person2.age);
+console.log(person2.isMale);
