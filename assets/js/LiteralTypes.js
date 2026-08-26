6d5b06f8bcda2f6885ec5292f2083f1c7a2e1e7f
ParsaSI <parsasi0101@gmail.com>
2026-08-26 17:56:07 +0330

    LiteralTypes

---
+++ assets/js/LiteralTypes.js
@@ -0,0 +1,22 @@
+"use strict";
+function combine2(input1, input2, resultType) {
+    let result;
+    if (typeof input1 == 'number' && typeof input2 == 'number') {
+        result = input1 + input2;
+    }
+    else {
+        result = input1.toString() + input2.toString();
+    }
+    if (resultType == "as-number") {
+        return +result;
+    }
+    else {
+        return result.toString();
+    }
+}
+const combinedAges2 = combine2(30, 26, "as-number");
+const combinedAges3 = combine2("30", "26", "as-text");
+const combinedNames2 = combine2('John ', 'Doe', "as-text");
+console.log(combinedAges2);
+console.log(combinedAges3);
+console.log(combinedNames2);
