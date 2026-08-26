f9060a6025f4379011a63b5566e68d7029c65ad1
ParsaSI <parsasi0101@gmail.com>
2026-08-26 17:31:22 +0330

    UnionTypes

---
+++ assets/js/UnionTypes.js
@@ -0,0 +1,15 @@
+"use strict";
+function combine(input1, input2) {
+    let result;
+    if (typeof input1 == 'number' && typeof input2 == 'number') {
+        result = input1 + input2;
+    }
+    else {
+        result = input1.toString() + input2.toString();
+    }
+    return result;
+}
+const combinedAges = combine(30, 26);
+const combinedNames = combine("John ", "Doe");
+console.log(combinedAges);
+console.log(combinedNames);
---
+++ assets/ts/UnionTypes.ts
@@ -0,0 +1,22 @@
+function combine(
+    input1: number | string,
+    input2: number | string
+): number | string {
+    let result: number | string;
+
+    if (typeof input1 == 'number' && typeof input2 == 'number') {
+        result = input1 + input2;
+    }else {
+        result = input1.toString() + input2.toString()
+    }
+
+    return result;
+}
+
+const combinedAges: number | string = combine(30, 26);
+const combinedNames: number | string = combine("John ", "Doe");
+
+
+
+console.log(combinedAges);
+console.log(combinedNames);
