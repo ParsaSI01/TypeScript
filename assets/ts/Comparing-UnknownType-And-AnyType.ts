dce1b9107b261de8ce1489ba7435e64a44db9bdf
ParsaSI <parsasi0101@gmail.com>
2026-09-14 01:30:35 +0330

    UnionTypes

---
+++ assets/ts/Comparing-UnknownType-And-AnyType.ts
@@ -0,0 +1,19 @@
+/*
+let userInput1: unknown;
+
+userInput1="Ali"
+userInput1=12
+userInput1=false
+userInput1=NaN
+ */
+
+let userInput1: unknown = 10;
+
+// let string1:string=userInput1;       // We Cant Do This With unknown
+// userInput1.method();
+
+let userInput2: any = 10;
+
+userInput2.method();
+
+let string2: string = userInput2;
